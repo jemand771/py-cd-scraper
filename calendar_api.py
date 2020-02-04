7e688db52f42a8278cf3dba544d9e1ea7bbeca64
Willy Hille <willy.hille@intenta.de>
2020-02-04 12:23:52 +0100

    i hate single quotes

--- calendar_api.py
+++ calendar_api.py
@@ -7,7 +7,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 
 class CalendarApi:
 
@@ -21,18 +21,18 @@ class CalendarApi:
     def __init__(self):
         
         creds = None
-        if os.path.exists('config/calendar-token.pickle'):
-            with open('config/calendar-token.pickle', 'rb') as token:
+        if os.path.exists("config/calendar-token.pickle"):
+            with open("config/calendar-token.pickle", "rb") as token:
                 creds = pickle.load(token)
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'config/calendar-api.json', SCOPES)
+                "config/calendar-api.json", SCOPES)
             creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('config/calendar-token.pickle', 'wb') as token:
+            with open("config/calendar-token.pickle", "wb") as token:
                 pickle.dump(creds, token)
 
-        self.service = build('calendar', 'v3', credentials=creds)
+        self.service = build("calendar", "v3", credentials=creds)
 
         f = open("config/calendar.json")
         cid = json.load(f)["cal_id"]
